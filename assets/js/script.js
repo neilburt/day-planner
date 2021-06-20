aba5db26bd4d6596818d614f664cf30dc34d3977
neilburt <neil.burt@comcast.net>
2021-06-20 15:27:58 -0500

    Completed hour row color-coding, fixed hour and button positioning.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-const currentDay = document.getElementById("currentDay");
+const currentDay = $('#currentDay');
+let hourEl = $('.hour');
+let hourNow = parseInt(moment().format('H'));
 
-currentDay.textContent = moment().format('MMMM Do YYYY, h:mm a');
+currentDay.text(moment().format('MMMM Do YYYY, h:mm a'));
+
+// live color-codes hour rows based on actual time
+hourEl.each(function() {
+  if(hourNow > parseInt($(this).attr("id"))) {
+    $(this).siblings().eq(0).addClass('past')
+  } else if(hourNow < parseInt($(this).attr("id"))) {
+    $(this).siblings().eq(0).addClass('future')
+  } else {
+    $(this).siblings().eq(0).addClass('present')
+  }
+});
+
