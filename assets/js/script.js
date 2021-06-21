06ee7df5b8c453715e0f976ffd7aa3b2a55ff4c7
neilburt <neil.burt@comcast.net>
2021-06-21 16:39:58 -0500

    Assigned id's to textareas in hopes of figuring out storage issue.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,44 @@ const currentDay = $('#currentDay');
 const hourEl = $('.hour');
 const hourNow = parseInt(moment().format('H'));
 const saveBtn = $('.saveBtn');
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+
+
+let planner;
+// renderPlanner();
 
 currentDay.text(moment().format('MMMM Do YYYY, h:mm a'));
 
+
+// makes clicking each hour row's button save the entry to local storage
 saveBtn.on('click', function(event) {
   event.preventDefault();
   let time = $(this).siblings().eq(0).text();
   let appt = $(this).siblings('.entry').val();
 
-  localStorage.setItem(time, appt);
+  localStorage.setItem("appointment" + time, appt);
 });
 
 // live color-codes hour rows based on actual time
 hourEl.each(function() {
-  if(hourNow > parseInt($(this).attr("id"))) {
+  if(hourNow > parseInt($(this).attr('id'))) {
     $(this).siblings().eq(0).addClass('past')
-  } else if(hourNow < parseInt($(this).attr("id"))) {
+  } else if(hourNow < parseInt($(this).attr('id'))) {
     $(this).siblings().eq(0).addClass('future')
   } else {
     $(this).siblings().eq(0).addClass('present')
   }
 });
 
-function init() {
-  
+function renderPlanner(appt) {
+ 
+
+
+ let getTextArea =  localStorage.getItem("appointment", appt);
+  console.log("text area",getTextArea);
 }
+
+//displayHours();
+
+renderPlanner();
