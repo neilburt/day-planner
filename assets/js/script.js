0bf2e8ca8e1d1af06f9c46c1ade9e48a473b72f7
neilburt <neil.burt@comcast.net>
2021-06-21 17:50:46 -0500

    Put in the script for loading local storage via textarea id's.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,30 @@ const hourNow = parseInt(moment().format('H'));
 const saveBtn = $('.saveBtn');
 var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-
-
-let planner;
-// renderPlanner();
-
 currentDay.text(moment().format('MMMM Do YYYY, h:mm a'));
 
+// loads any entries existing in local storage
+function renderPlanner() {
+  $('#9t').val(localStorage.getItem("9 am"));
+  $('#10t').val(localStorage.getItem("10 am"));
+  $('#11t').val(localStorage.getItem("11 am"));
+  $('#12t').val(localStorage.getItem("12 pm"));
+  $('#1t').val(localStorage.getItem("1 pm"));
+  $('#2t').val(localStorage.getItem("2 pm"));
+  $('#3t').val(localStorage.getItem("3 pm"));
+  $('#4t').val(localStorage.getItem("4 pm"));
+  $('#5t').val(localStorage.getItem("5 pm"));
+}
+renderPlanner();
 
 // makes clicking each hour row's button save the entry to local storage
 saveBtn.on('click', function(event) {
   event.preventDefault();
+
   let time = $(this).siblings().eq(0).text();
   let appt = $(this).siblings('.entry').val();
 
-  localStorage.setItem("appointment" + time, appt);
+  localStorage.setItem(time, appt);
 });
 
 // live color-codes hour rows based on actual time
@@ -31,15 +40,3 @@ hourEl.each(function() {
     $(this).siblings().eq(0).addClass('present')
   }
 });
-
-function renderPlanner(appt) {
- 
-
-
- let getTextArea =  localStorage.getItem("appointment", appt);
-  console.log("text area",getTextArea);
-}
-
-//displayHours();
-
-renderPlanner();
